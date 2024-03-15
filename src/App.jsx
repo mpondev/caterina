da9e235713305458827831469e7afa250ca6229e
Mario Ponce <mario@mpondev.com>
2024-03-15 15:48:57 +0100

    Add react-router and set up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: 'bookings',
+    element: <Bookings />,
+  },
+  {
+    path: 'cabins',
+    element: <Cabins />,
+  },
+  {
+    path: 'users',
+    element: <Users />,
+  },
+  {
+    path: 'settings',
+    element: <Settings />,
+  },
+  {
+    path: 'account',
+    element: <Account />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+]);
+
 function App() {
-  return <div>Apartamentos Santa Catalina</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
