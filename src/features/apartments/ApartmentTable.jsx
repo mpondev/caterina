f0ee03c45db1c50f173539a5598383aa286bb514
Mario Ponce <mario@mpondev.com>
2024-03-19 19:44:25 +0100

    Add ApartmentTable component

---
+++ src/features/apartments/ApartmentTable.jsx
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getApartments } from '../../services/apiApartments';
+import ApartmentRow from './ApartmentRow';
+import Spinner from '../../ui/Spinner';
+
+import './ApartmentTable.scss';
+
+function ApartmentTable() {
+  const {
+    isLoading,
+    data: apartments,
+    error,
+  } = useQuery({
+    queryKey: ['apartment'],
+    queryFn: getApartments,
+  });
+  console.log(apartments, error);
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <div className="apartmentTable" role="table">
+      <header className="apartmentHeader" role="row">
+        <div></div>
+        <div>Apartamento</div>
+        <div>Capacidad</div>
+        <div>Precio</div>
+        <div>Descuento</div>
+        <div></div>
+      </header>
+      {apartments.map(apartment => (
+        <ApartmentRow apartment={apartment} key={apartment.id} />
+      ))}
+    </div>
+  );
+}
+
+export default ApartmentTable;
