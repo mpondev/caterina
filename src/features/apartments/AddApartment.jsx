384755ca9bc4575055bb83e71fd42cfc97427241
Mario Ponce <mario@mpondev.com>
2024-03-23 18:52:37 +0100

    Add Modal to Apartment edition

--- src/features/apartments/AddApartment.jsx
+++ src/features/apartments/AddApartment.jsx
@@ -1,4 +1,4 @@
-import ApartmentTable from './ApartmentTable';
+// import ApartmentTable from './ApartmentTable';
 import CreateApartmentForm from './CreateApartmentForm';
 import Modal from '../../ui/Modal';
 
@@ -6,39 +6,17 @@ import './AddApartment.scss';
 
 function AddApartment() {
   return (
-    <Modal>
-      <Modal.Open opens={'apartment-form'}>
-        <button className="modal-btn">Añadir Apartamento</button>
-      </Modal.Open>
-      <Modal.Window name="apartment-form">
-        <CreateApartmentForm />
-      </Modal.Window>
-
-      <Modal.Open opens={'table'}>
-        <button className="modal-btn">Abrir Tabla</button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <ApartmentTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens={'apartment-form'}>
+          <button className="modal-btn">Añadir Apartamento</button>
+        </Modal.Open>
+        <Modal.Window name="apartment-form">
+          <CreateApartmentForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
-
-// function AddApartment() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-
-//   return (
-//     <div className="row">
-//       <button onClick={() => setIsOpenModal(show => !show)}>
-//         Añadir Apartamento
-//       </button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateApartmentForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddApartment;
--- src/features/apartments/ApartmentRow.jsx
+++ src/features/apartments/ApartmentRow.jsx
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 
 import { useCreateApartment } from './useCreateApartment';
 import { useDeleteApartment } from './useDeleteApartment';
 import CreateApartmentForm from './CreateApartmentForm';
+import Modal from '../../ui/Modal';
 import { formatCurrency } from '../../utils/helpers';
 
 import './ApartmentRow.scss';
 
 function ApartmentRow({ apartment }) {
-  const [showForm, setShowForm] = useState(false);
   const { deleteApartment, isDeleting } = useDeleteApartment();
   const { createApartment, isCreating } = useCreateApartment();
 
@@ -36,38 +35,40 @@ function ApartmentRow({ apartment }) {
   }
 
   return (
-    <>
-      <div className="apartmentRow" role="row">
-        <img src={image} alt="apartment image" className="apartmentRow--img" />
-        <div className="apartmentRow--name">{apartmentName}</div>
-        <div>Hasta {max_capacity} personas</div>
-        <div className="apartmentRow--price">
-          {formatCurrency(regular_price)}
-        </div>
-        {discount ? (
-          <div className="apartmentRow--discount">
-            {formatCurrency(discount)}
-          </div>
-        ) : (
-          <span>&mdash;</span>
-        )}
-        <div>
-          <button onClick={handleDuplicate} disabled={isCreating}>
-            <HiSquare2Stack />
-          </button>
-          <button onClick={() => setShowForm(!showForm)}>
-            <HiPencil />
-          </button>
+    <div className="apartmentRow" role="row">
+      <img src={image} alt="apartment image" className="apartmentRow--img" />
+      <div className="apartmentRow--name">{apartmentName}</div>
+      <div>Hasta {max_capacity} personas</div>
+      <div className="apartmentRow--price">{formatCurrency(regular_price)}</div>
+      {discount ? (
+        <div className="apartmentRow--discount">{formatCurrency(discount)}</div>
+      ) : (
+        <span>&mdash;</span>
+      )}
+
+      <div>
+        <button onClick={handleDuplicate} disabled={isCreating}>
+          <HiSquare2Stack />
+        </button>
+
+        <Modal>
+          <Modal.Open opens="edit">
+            <button>
+              <HiPencil />
+            </button>
+          </Modal.Open>
+          <Modal.Window name="edit">
+            <CreateApartmentForm apartmentToEdit={apartment} />
+          </Modal.Window>
           <button
             onClick={() => deleteApartment(apartmentId)}
             disabled={isDeleting}
           >
             <HiTrash />
           </button>
-        </div>
+        </Modal>
       </div>
-      {showForm && <CreateApartmentForm apartmentToEdit={apartment} />}
-    </>
+    </div>
   );
 }
 
