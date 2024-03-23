9f45df10714b61318fc2d857e0beddf7d5a7343a
Mario Ponce <mario@mpondev.com>
2024-03-23 17:36:03 +0100

    Convert Modal to a Compound Component

--- src/features/apartments/AddApartment.jsx
+++ src/features/apartments/AddApartment.jsx
@@ -1,24 +1,44 @@
-import { useState } from 'react';
+import ApartmentTable from './ApartmentTable';
 import CreateApartmentForm from './CreateApartmentForm';
 import Modal from '../../ui/Modal';
 
 import './AddApartment.scss';
 
 function AddApartment() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div className="row">
-      <button onClick={() => setIsOpenModal(show => !show)}>
-        Añadir Apartamento
-      </button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateApartmentForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens={'apartment-form'}>
+        <button className="modal-btn">Añadir Apartamento</button>
+      </Modal.Open>
+      <Modal.Window name="apartment-form">
+        <CreateApartmentForm />
+      </Modal.Window>
+
+      <Modal.Open opens={'table'}>
+        <button className="modal-btn">Abrir Tabla</button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <ApartmentTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddApartment() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div className="row">
+//       <button onClick={() => setIsOpenModal(show => !show)}>
+//         Añadir Apartamento
+//       </button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateApartmentForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddApartment;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,17 +1,42 @@
 import PropTypes from 'prop-types';
+import { cloneElement, createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 
 import './Modal.scss';
 
-function Modal({ children, onClose }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ close, open, openName }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  if (name !== openName) return null;
+
   return createPortal(
     <div className="overlay">
       <div className="modal">
-        <button className="close-btn" onClick={onClose}>
+        <button className="close-btn" onClick={close}>
           <HiXMark className="icon" />
         </button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </div>
     </div>,
     document.body
@@ -19,8 +44,16 @@ function Modal({ children, onClose }) {
 }
 
 Modal.propTypes = {
-  children: PropTypes.object,
+  children: PropTypes.array,
   onClose: PropTypes.func,
 };
+
+Window.propTypes = {
+  children: PropTypes.object,
+  name: PropTypes.string,
+};
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
