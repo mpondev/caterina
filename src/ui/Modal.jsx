8f9f08e9b540d80df9b9bc76e6aebe0671a4b708
Mario Ponce <mario@mpondev.com>
2024-03-23 18:18:45 +0100

    Detect a click outside the Modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,12 @@
 import PropTypes from 'prop-types';
-import { cloneElement, createContext, useContext, useState } from 'react';
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 
@@ -28,11 +35,22 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(evt) {
+      if (ref.current && !ref.current.contains(evt.target)) close();
+    }
+    document.addEventListener('click', handleClick, true);
+
+    return () => document.removeEventListener('click', handleClick, true);
+  }, [close]);
+
   if (name !== openName) return null;
 
   return createPortal(
     <div className="overlay">
-      <div className="modal">
+      <div className="modal" ref={ref}>
         <button className="close-btn" onClick={close}>
           <HiXMark className="icon" />
         </button>
